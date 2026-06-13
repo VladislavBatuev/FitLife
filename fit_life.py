708ccb30c0b98e8ab1c00b07b682bbab414a8223
Vladislav Batuev <vlad1slav.batuev@yandex.ru>
2026-06-13 17:02:05 +0300

    Написан код MVP

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,39 @@
-# Проект FitLife - MVP версия 1.0
+import sys
 
 
-# 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stdin.reconfigure(encoding='utf-8')
+
+def greetings():  # функция обрабатывающая Имя и Возраст
+    user_name = input('Введите своё имя пользователя: ')
+    user_name = user_name.title()
+    user_age = int(input('Теперь просим ввести возраст пользователя: '))
+    print('-' * 45)
+    return user_name, user_age
 
 
-# 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
+def bmi_calc():  # функция для расчётов
+    print('Просим вводить следующие значения через точку (например, рост(м): 1.82)')
+    print()
+    user_weight = float(input('Введите ваш вес(кг): '))
+    user_height = float(input('Введите ваш рост(м): '))
+    bmi = user_weight / (user_height ** 2)  # расчёт индекса массы тела
+    bmi = round(bmi, 1)
+    water_ml = user_weight * 30
+    water_l = water_ml / 1000
+    water_l = round(water_l, 2)
+    return bmi, water_l
 
-
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
-
-
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
-
-
-# 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-print("Расчет окончен. Будьте здоровы!")
+print('\nДобро пожаловать! Вас приветствует FitLife!')
+name, age = greetings()
+bmi, water = bmi_calc()
+print('-' * 45)
+print('\n' + '=' * 35)
+print('ВАШ ОТЧЁТ')
+print('=' * 35)
+print(f'Имя: {name}') 
+print(f'Возраст: {age}') 
+print(f'ИМТ: {bmi}')
+print(f'Ваша норма воды составляет: {water}')
+print('-' * 45)
+print('Расчёт окончен. Будьте здоровы!')
