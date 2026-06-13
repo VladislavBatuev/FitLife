01a578d94ff8c612ca118ddd2929c998c8089e90
Vladislav Batuev <vlad1slav.batuev@yandex.ru>
2026-06-13 17:17:13 +0300

    Исправлен код в соответствии с PEP8

--- fit_life.py
+++ fit_life.py
@@ -1,10 +1,11 @@
 import sys
 
-
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stdin.reconfigure(encoding='utf-8')
 
-def greetings():  # функция обрабатывающая Имя и Возраст
+
+def greetings():
+    """Обрабатывает ввод имени и возраста пользователя."""
     user_name = input('Введите своё имя пользователя: ')
     user_name = user_name.title()
     user_age = int(input('Теперь просим ввести возраст пользователя: '))
@@ -12,17 +13,19 @@ def greetings():  # функция обрабатывающая Имя и Воз
     return user_name, user_age
 
 
-def bmi_calc():  # функция для расчётов
-    print('Просим вводить следующие значения через точку (например, рост(м): 1.82)')
+def bmi_calc():
+    """Рассчитывает ИМТ и норму воды."""
+    print('Просим вводить значения через точку (например, рост(м): 1.82)')
     print()
     user_weight = float(input('Введите ваш вес(кг): '))
     user_height = float(input('Введите ваш рост(м): '))
-    bmi = user_weight / (user_height ** 2)  # расчёт индекса массы тела
+    bmi = user_weight / (user_height ** 2)
     bmi = round(bmi, 1)
     water_ml = user_weight * 30
     water_l = water_ml / 1000
     water_l = round(water_l, 2)
     return bmi, water_l
+
 
 print('\nДобро пожаловать! Вас приветствует FitLife!')
 name, age = greetings()
@@ -31,8 +34,8 @@ print('-' * 45)
 print('\n' + '=' * 35)
 print('ВАШ ОТЧЁТ')
 print('=' * 35)
-print(f'Имя: {name}') 
-print(f'Возраст: {age}') 
+print(f'Имя: {name}')
+print(f'Возраст: {age}')
 print(f'ИМТ: {bmi}')
 print(f'Ваша норма воды составляет: {water}')
 print('-' * 45)
